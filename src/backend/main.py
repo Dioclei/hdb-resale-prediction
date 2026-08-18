57726d1ddcde35097f283af7001d2dd54ae9db89
Jason Lee <jasonepic0801@gmail.com>
2026-08-18 15:46:03 +0800

    refactor FastAPI backend: add a lifespan, add dependencies

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,34 +1,51 @@
 import atexit
 import logging
 import random
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Request, Depends, Query
 from typing import Annotated
+from contextlib import asynccontextmanager
 
 from backend.model import LinearRegressionModel
 from backend.config.logging_config import LOGGING_CONFIG
 from backend.schemas import LinearRegressionModel_Input
-
-# Set up logging and log queue handler process
-def setup_logging():
-    logging.config.dictConfig(LOGGING_CONFIG)
-    queue_handler = logging.getHandlerByName("queue_handler")
-    if queue_handler is not None:
-        queue_handler.listener.start()
-        atexit.register(queue_handler.listener.stop)
-setup_logging()
-logger = logging.getLogger(__name__)
-
-# Set up models
-lr = LinearRegressionModel()
-
-# Set up API server
-app = FastAPI()
 
 def get_request_id():
     # there will be collisions but it should be very rare for simple debugging purposes.
     # if used for more complex purposes, use a better id generation method.
     randint = random.randint(1, 99999999)
     return f"R-{randint}"
+
+# FastAPI Lifespan
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # FastAPI App Startup
+
+    # Set up logging and log queue handler process
+    logging.config.dictConfig(LOGGING_CONFIG)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+    app.state.logger = logging.getLogger(__name__)
+
+    # Set up models
+    app.state.lr = LinearRegressionModel()
+
+    yield
+
+    # FastAPI App Shutdown
+    
+    # Shut down logging queue handler process
+    if queue_handler is not None:
+        queue_handler.listener.stop()
+
+# FastAPI Dependencies
+async def get_logger(request: Request):
+    return request.app.state.logger
+async def get_lr(request: Request):
+    return request.app.state.lr
+
+# Set up API server
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
@@ -40,7 +57,10 @@ async def get_inference(
     input_features: Annotated[LinearRegressionModel_Input, Query(
         title="Linear Regression Model Input Features",
         description="All features (date, floor_area_sqm, town, flat_type) are required."
-    )]):
+    )],
+    logger: Annotated[Request, Depends(get_logger)],
+    lr: Annotated[Request, Depends(get_lr)],
+):
     req_id = get_request_id()
     logger.info("Inference request", extra={
         "request_id": req_id,
